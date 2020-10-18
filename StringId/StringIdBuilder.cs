2e265174be995be4b3d73b17361bb5c53b3d3ae6
Adam O'Neil <adamosoftware@gmail.com>
2020-10-18 04:00:55 -0400

    Add project files.

---
+++ StringId/StringIdBuilder.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StringId.Library
+{
+    [Flags]
+    public enum StringIdRanges
+    {
+        Lower = 1,
+        Upper = 2,
+        Numeric = 4,
+        Special = 8
+    }
+
+    /// <summary>
+    /// this is a way to generate random string Ids of a desired length and format
+    /// </summary>
+    public static class StringId
+    {
+        // idea from https://scottlilly.com/create-better-random-numbers-in-c/
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        private static Dictionary<StringIdRanges, Dictionary<byte, char>> _charMaps = new Dictionary<StringIdRanges, Dictionary<byte, char>>();
+
+        public static string New(int length, StringIdRanges charRanges = StringIdRanges.Lower | StringIdRanges.Numeric)
+        {
+            var ranges = new Dictionary<StringIdRanges, string>()
+            {
+                [StringIdRanges.Lower] = "abcdefghijklmnopqrstuvwxyz",
+                [StringIdRanges.Upper] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                [StringIdRanges.Numeric] = "1234567890",
+                [StringIdRanges.Special] = ".<>?!@#$%^&*()"
+            };
+
+            string source = string.Join(string.Empty, ranges.Where(r => (charRanges & r.Key) == r.Key).Select(kp => kp.Value));
+            var sourceBytes = Encoding.ASCII.GetBytes(source);
+
+            if (!_charMaps.ContainsKey(charRanges))
+            {
+                _charMaps.Add(charRanges, CreateByteCharMap(source));
+            }
+
+            var charMap = _charMaps[charRanges];
+
+            StringBuilder result = new StringBuilder();
+            
+            byte[] randomBytes = new byte[length - result.Length];
+            _generator.GetNonZeroBytes(randomBytes);
+            for (int i = 0; i < length; i++) result.Append(charMap[randomBytes[i]]);               
+            return result.ToString();
+        }
+
+        private static Dictionary<byte, char> CreateByteCharMap(string source)
+        {
+            var charArray = source.ToCharArray();
+
+            int index = 0;
+            return Enumerable.Range(1, byte.MaxValue)
+                .Select(i => new 
+                { 
+                    ByteValue = (byte)i, 
+                    Character = getNextChar() 
+                })
+                .ToDictionary(val => val.ByteValue, val => val.Character);
+            
+            char getNextChar()
+            {
+                char result = charArray[index];
+                index++;
+                if (index > charArray.Length - 1) index = 0;
+                return result;
+            }
+        }
+    }
+
+    public class StringIdBuilder
+    {
+        private StringBuilder _result = new StringBuilder();
+
+        public StringIdBuilder Add(int length, StringIdRanges charRanges)
+        {
+            _result.Append(StringId.New(length, charRanges));
+            return this;
+        }
+
+        public StringIdBuilder Add(string literal)
+        {
+            _result.Append(literal);
+            return this;
+        }
+
+        public string Build() => _result.ToString();
+    }
+}
---
+++ Testing/StringIdTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringId.Library
+{
+    [TestClass]
+    public class StringIdTests
+    {
+        [TestMethod]
+        public void RandomIdsAreUnique()
+        {
+            HashSet<string> test = new HashSet<string>();
+
+            const int iterations = 1_000_000;
+            for (int i = 0; i < iterations; i++) test.Add(StringId.New(9));
+
+            // if the generated Ids are unique enough, then the hash set count will equal the iteration count
+            Assert.IsTrue(test.Count == iterations);
+            Assert.IsTrue(test.All(id => id.Length == 9));
+
+            // We're assuming that a million 9-character Ids should be unique.
+            // We'd likely have to tune this assumption for different Id lengths and number of iterations
+        }
+
+        [TestMethod]
+        public void ChainResults()
+        {
+            string result = new StringIdBuilder()
+                .Add(4, StringIdRanges.Upper)
+                .Add("-")
+                .Add(4, StringIdRanges.Upper)
+                .Add("-")
+                .Add(4, StringIdRanges.Upper)
+                .Build();
+
+            Assert.IsTrue(result.Length == 14);
+        }
+    }
+}
